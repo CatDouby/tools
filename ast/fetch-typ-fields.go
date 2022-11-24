3a293c04cc5092307b5f2d369b7bbb3b6dab7681
yhuaf <yhuaf@newmoons.cn>
2022-11-24 19:17:43 +0800

    .

---
+++ ast/fetch-typ-fields.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/CatDouby/tools/def"
+)
+
+type NerPerson struct {
+	def.Person
+	Phone string
+}
+
+func main() {
+
+}
---
+++ ast/go.mod
@@ -0,0 +1,3 @@
+module go-tools/ast
+
+go 1.18
